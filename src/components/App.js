6903dd2afca32a7520695594efc0ac02f3be096e
ochieng <cindycynthia387@gmail.com>
2023-04-23 13:43:04 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
   return (
+    
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+
+      <Header name = "Underreacted"/>
+
+      <About image = "https://via.placeholder.com/215" about = "React Learning Blog."/>
+     
+      <ArticleList posts = {blogData.posts}/>
     </div>
   );
 }
